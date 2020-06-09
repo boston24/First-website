84fb8955a99afa1870a691b5d883c0223d56db44
Mateusz Jendernal <matij99@gmail.com>
2020-06-09 22:56:08 +0200

    upload all files

---
+++ standings.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8">
+	<title>Project</title>
+	<link rel="stylesheet" href="style1.css" type="text/css" /> 
+	<link rel="stylesheet" href="css/fontello.css" type="text/css" /> 
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+	
+	<script src="zegar.js"></script>
+
+
+</head>
+
+<body>
+
+
+	<header>
+		
+		<a href="intex.php" class="linklogo"><h1 class="logo">Basketball news</h1></a>
+		
+			<nav>
+					
+					<ul class="menu">
+						<li><a href="index.php" class="linknav">Home</a></li>
+						<li><a href="standings.php" class="linknav">Standings</a></li>
+						<li><a href="stats.php" class="linknav">Stats</a></li>
+						<li><a href="https://stats.nba.com/schedule/" class="linknav">Schedule</a></li>
+					</ul>
+			</nav>			
+		
+	</header>
+	
+	
+		<article>
+		
+			<header id="fel">
+			
+			<h1>Easter Conference Standings</h1>
+			
+			</header>
+			
+			<section>
+			
+				<table> 
+				 
+					 <tr>
+					 <th>Team</th> 
+					 <th>W</th>
+					 <th>L</th>
+					 </tr>
+					 
+					 <tr> 
+					 <td>Bucks</td> 
+					 <td>34</td>
+					 <td>12</td>
+					 </tr>
+					 
+					 <tr> 
+					 <td>Raptors</td>
+					 <td>36</td>
+					 <td>14</td>
+					 </tr>
+					 
+					 <tr> 
+					 <td>Pacers</td>
+					 <td>32</td>
+					 <td>15</td>
+					 </tr>
+					 
+					 <tr> 
+					 <td>76ers</td>
+					 <td>32</td>
+					 <td>17</td>
+					 </tr>
+					 
+					 <tr> 
+					 <td>Celtics</td>
+					 <td>30</td>
+					 <td>18</td>
+					 </tr>
+					 
+				</table> 
+			
+	</article>
+	
+	<aside>
+		Find us here:
+		<div class="yt">
+			<a href="http://www.youtube.com/?gl=PL&hl=pl" class="social" target="_blank"><i class="icon-youtube"></i></a>
+		</div>
+		
+		<div class="fb">
+			<a href="http://www.facebook.com/" class="social" target="_blank"><i class="icon-facebook" ></i></a>
+		</div>
+		
+		<div class="ig">
+			<a href="https://www.instagram.com/" class="social" target="_blank"><i class="demo-icon icon-instagram"></i></a>
+		</div>
+		
+		<div class="tw">
+			<a href="https://twitter.com/?lang=pl" class="social" target="_blank"><i class="demo-icon icon-twitter"></i></a>
+		</div>
+		
+	</aside>
+		
+	<footer>
+	2019  | Mateusz Jendernal
+	</footer>
+	
+		
+			
+
+
+</body>
---
+++ stats.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+	<meta charset="utf-8">
+	<title>Project</title>
+	<link rel="stylesheet" href="style1.css" type="text/css" /> 
+	<link rel="stylesheet" href="css/fontello.css" type="text/css" /> 
+	<link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Staatliches" rel="stylesheet">
+	
+	<script src="zegar.js"></script>
+
+</head>
+
+<body>
+
+
+	<header>
+		
+		<a href="index.php" class="linklogo"><h1 class="logo">Basketball news</h1></a>
+		
+			<nav>
+					
+					<ul class="menu">
+						<li><a href="index.php" class="linknav">Home</a></li>
+						<li><a href="standings.php" class="linknav">Standings</a></li>
+						<li><a href="stats.php" class="linknav">Stats</a></li>
+						<li><a href="https://stats.nba.com/schedule/" class="linknav">Schedule</a></li>
+					</ul>
+			</nav>			
+		
+	</header>
+	
+	
+		<article>
+		
+			<script>
+			odliczanie();
+			</script>
+		
+			<header id="fel">
+			
+			<h1>Recent games</h1>
+			
+			</header>
+			
+			<figure>
+			<img src="scores.jpg" />
+			</figure>
+
+			
+				
+	</article>
+	
+	<aside>
+		Find us here:
+		<div class="yt">
+			<a href="http://www.youtube.com/?gl=PL&hl=pl" class="social" target="_blank"><i class="icon-youtube"></i></a>
+		</div>
+		
+		<div class="fb">
+			<a href="http://www.facebook.com/" class="social" target="_blank"><i class="icon-facebook" ></i></a>
+		</div>
+		
+		<div class="ig">
+			<a href="https://www.instagram.com/" class="social" target="_blank"><i class="demo-icon icon-instagram"></i></a>
+		</div>
+		
+		<div class="tw">
+			<a href="https://twitter.com/?lang=pl" class="social" target="_blank"><i class="demo-icon icon-twitter"></i></a>
+		</div>
+		
+	</aside>
+		
+	<footer>
+	2019  | Mateusz Jendernal
+	</footer>
+	
+		
+			
+
+
+</body>
